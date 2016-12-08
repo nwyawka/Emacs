267ad3105954ef4cd937431f183dfa775eb503d0
nwyawka <nwyawka@gmail.com>
2016-12-07 23:56:49 -0500

    reinitialized

---
+++ elpa/htmlize-20161205.1201/htmlize-pkg.el
@@ -0,0 +1,1 @@
+(define-package "htmlize" "20161205.1201" "Convert buffer text and decorations to HTML." 'nil :keywords '("hypermedia" "extensions"))
---
+++ elpa/websocket-20161113.1736/websocket-autoloads.el
@@ -0,0 +1,15 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("websocket.el") (22598 18241 743930 496000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; websocket-autoloads.el ends here
